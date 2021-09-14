faf1a3c099e03bc9ba83a234ec6c9f36c5e2b5ee
cariella <90623590+cariella@users.noreply.github.com>
2021-09-13 20:42:25 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,20 +1,28 @@
+//setting variables for the color...
 let color = document.getElementById("colorPicker");
+//for the height and width picked by the user...
 let gridHeight = document.getElementById("inputHeight");
 let gridWidth = document.getElementById("inputWidth");
+//and for the size of the grid based on the height and width
 let size = document.getElementById("sizePicker");
 let userGrid = document.getElementById("pixelCanvas");
 
+//adding event listener so the height and width can be changed by the user
 size.addEventListener("submit", function (event)
   {console.log("asdfsadf");
   userGrid.innerHTML = "";
+//event.preventDefault() stops the height and width from "defaulting" to 1
   event.preventDefault();
+//calling function to make the grid per the height and width
   makeGrid(gridHeight.value, gridWidth.value);});
 
-  function makeGrid(gridHeight, gridWidth)
-    {for (y = 0; y < gridHeight; y++)
-      {var row = userGrid.insertRow(y);
-      for (x = 0; x < gridWidth; x++)
-        {var cell = row.insertCell(x);}}
+function makeGrid(gridHeight, gridWidth)
+//looping over to insert rows and cells
+  {for (y = 0; y < gridHeight; y++)
+    {var row = userGrid.insertRow(y);
+    for (x = 0; x < gridWidth; x++)
+      {var cell = row.insertCell(x);}}
 
-  userGrid.addEventListener("click", function (event)
-    {event.target.style.backgroundColor = color.value;});}
+//letting the user pick a color for their masterpiece
+userGrid.addEventListener("click", function (event)
+  {event.target.style.backgroundColor = color.value;});}
