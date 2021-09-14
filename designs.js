51cd43bbcd0509028e25d1add9478b0d07795d95
cariella <90623590+cariella@users.noreply.github.com>
2021-09-13 20:45:49 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -8,21 +8,27 @@ let size = document.getElementById("sizePicker");
 let userGrid = document.getElementById("pixelCanvas");
 
 //adding event listener so the height and width can be changed by the user
-size.addEventListener("submit", function (event)
-  {console.log("asdfsadf");
-  userGrid.innerHTML = "";
-//event.preventDefault() stops the height and width from "defaulting" to 1
-  event.preventDefault();
-//calling function to make the grid per the height and width
-  makeGrid(gridHeight.value, gridWidth.value);});
+size.addEventListener("submit", function(event) {
+    console.log("asdfsadf");
+    userGrid.innerHTML = "";
+    //event.preventDefault() stops the height and width from "defaulting" to 1
+    event.preventDefault();
+    //calling function to make the grid per the height and width
+    makeGrid(gridHeight.value, gridWidth.value);
+});
 
 function makeGrid(gridHeight, gridWidth)
 //looping over to insert rows and cells
-  {for (y = 0; y < gridHeight; y++)
-    {var row = userGrid.insertRow(y);
-    for (x = 0; x < gridWidth; x++)
-      {var cell = row.insertCell(x);}}
+{
+    for (y = 0; y < gridHeight; y++) {
+        var row = userGrid.insertRow(y);
+        for (x = 0; x < gridWidth; x++) {
+            var cell = row.insertCell(x);
+        }
+    }
 
-//letting the user pick a color for their masterpiece
-userGrid.addEventListener("click", function (event)
-  {event.target.style.backgroundColor = color.value;});}
+    //letting the user pick a color for their masterpiece
+    userGrid.addEventListener("click", function(event) {
+        event.target.style.backgroundColor = color.value;
+    });
+}
